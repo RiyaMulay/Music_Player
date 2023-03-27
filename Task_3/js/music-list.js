3e5da131064d9393326d0e6edeb65082aa364630
RiyaMulay <85435717+RiyaMulay@users.noreply.github.com>
2023-03-27 23:57:13 +0530

    Add files via upload

---
+++ Task_3/js/music-list.js
@@ -0,0 +1,33 @@
+let allMusic = [
+  {
+    name: "Maan Meri Jaan ",
+    artist: "King",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Apna Bana Le",
+    artist: "Arjit Singh",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Sun Maahi",
+    artist: "Armaan Malik",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Jhoome Jo Pathaan",
+    artist: "Arijit Singh",
+    img: "music-4",
+    src: "music-4"
+  },
+
+  {
+    name: "Kesariya (Brahmastra)",
+    artist: "Arijit Singh",
+    img: "music-5",
+    src: "music-5"
+  }
+];
